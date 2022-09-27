a80647afffa2295df6df45523fca49001d4e466d
HP <ihsanakcair@gmail.com>
2022-09-27 22:18:56 +0300

    Initial commit

---
+++ src/day_1/aaa.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class aaa {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day_2/bbb.java
@@ -0,0 +1,7 @@
+package day_2;
+
+public class bbb {
+    public static void main(String[] args) {
+        System.out.println("fff");
+    }
+}
